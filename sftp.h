b2f7ddfb3699cfa4929a86a3a35cd6846225bd84
licaceres <licaceres04@gmail.com>
2019-04-15 01:10:53 -0300

    Se completa la función check_credentials. Se agrega header con funciones adicionales.

---
+++ sftp.h
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SFTP_H_INCLUDED
+#define SFTP_H_INCLUDED
+
+#define LINEMAX 30
+
+bool busqEnArchivo(char *credentials, FILE *file)
+{
+    char* line = (char*) malloc (sizeof(char) * LINEMAX);
+    int i = 0;
+
+    fgets(line, LINEMAX, file);
+
+    while (!feof(file))
+    {
+        i++;
+        if (!strcmp(credentials, line))
+        {
+            return true;
+        }
+        fgets(line, LINEMAX, file);
+    }
+    free (line);
+    return false;
+}
+
+#endif // SFTP_H_INCLUDED
